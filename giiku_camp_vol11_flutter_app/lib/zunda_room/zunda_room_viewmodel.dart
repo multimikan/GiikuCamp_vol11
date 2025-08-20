ec6322f63a2fd9edf1576ed9a81bed2fb39c9116
multimikan <koki20041008@gmail.com>
2025-08-20 19:43:06 +0900

    いちじじ中断

--- giiku_camp_vol11_flutter_app/lib/zunda_room/zunda_room_viewmodel.dart
+++ giiku_camp_vol11_flutter_app/lib/zunda_room/zunda_room_viewmodel.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-enum Walk{
+enum Status{
   walk,
   cry,
   bad,
@@ -11,22 +13,50 @@ enum Walk{
 }
 
 class ZundaRoomViewModel extends ChangeNotifier{
-  List<Image> _getAnimationImages(Walk walk){
-    switch(walk){
-      case (Walk.walk):
-        return [Image.asset("images/ZUNDA/zundamon1"),Image.asset("images/ZUNDA/zundamon2")];
-      case (Walk.cry):
-        return [Image.asset("images/ZUNDA/zundamon23"),Image.asset("images/ZUNDA/zundamon24")];
-      case (Walk.bad):
-        return [Image.asset("images/ZUNDA/zundamon27"),Image.asset("images/ZUNDA/zundamon30")];
-      case (Walk.surprize1):
-        return [Image.asset("images/ZUNDA/zundamon1"),Image.asset("images/ZUNDA/zundamon4")];
-      case (Walk.surprize2):
-        return [Image.asset("images/ZUNDA/zundamon1"),Image.asset("images/ZUNDA/zundamon12")];
-      case (Walk.surprize3):
-        return [Image.asset("images/ZUNDA/zundamon1"),Image.asset("images/ZUNDA/zundamon21")];
-      case (Walk.look):
-        return [Image.asset("images/ZUNDA/zundamon17"),Image.asset("images/ZUNDA/zundamon18")];
+  bool _showFirst = true;
+  bool get showFirst => _showFirst; //getter
+
+  ZundaRoomViewModel(){
+    Timer.periodic(Duration(microseconds: 500), (_) {
+      _showFirst = !_showFirst; //0.5sごとにshowFirstが切り替わる
+      notifyListeners();
+    });
+  }
+
+  List<Image> getAnimationImages(Status status){
+    switch(status){
+      case (Status.walk):
+        return [Image.asset("images/ZUNDA/zundamon1",key: const ValueKey(1)),Image.asset("images/ZUNDA/zundamon2",key: const ValueKey(2))];
+      case (Status.cry):
+        return [Image.asset("images/ZUNDA/zundamon23",key: const ValueKey(1)),Image.asset("images/ZUNDA/zundamon24",key: const ValueKey(2))];
+      case (Status.bad):
+        return [Image.asset("images/ZUNDA/zundamon27",key: const ValueKey(1)),Image.asset("images/ZUNDA/zundamon30",key: const ValueKey(2))];
+      case (Status.surprize1):
+        return [Image.asset("images/ZUNDA/zundamon1",key: const ValueKey(1)),Image.asset("images/ZUNDA/zundamon4",key: const ValueKey(2))];
+      case (Status.surprize2):
+        return [Image.asset("images/ZUNDA/zundamon1",key: const ValueKey(1)),Image.asset("images/ZUNDA/zundamon12",key: const ValueKey(2))];
+      case (Status.surprize3):
+        return [Image.asset("images/ZUNDA/zundamon1",key: const ValueKey(1)),Image.asset("images/ZUNDA/zundamon21",key: const ValueKey(2))];
+      case (Status.look):
+        return [Image.asset("images/ZUNDA/zundamon17",key: const ValueKey(1)),Image.asset("images/ZUNDA/zundamon18",key: const ValueKey(2))];
     }
   }
 }
+
+/*viewで実装
+class _MyAnimatedImageState extends State<MyAnimatedImage> {
+  final vm = ZundaRoomView
+
+  @override
+  Widget build(BuildContext context) {
+    final animevm = context.watch<ZundaRoomViewmodel>();
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 300),
+      child: animevm.showFirst
+          ? Image.asset('assets/image1.png', key: const ValueKey(1))
+          : Image.asset('assets/image2.png', key: const ValueKey(2)),
+      ),
+    );
+  }
+}
+*/
