48ccde5fccfd1e7034be3bbe37f7fa475bd00e4a
multimikan <koki20041008@gmail.com>
2025-08-21 23:49:02 +0900

    Update zunda_room_viewmodel.dart

--- giiku_camp_vol11_flutter_app/lib/zunda_room/zunda_room_viewmodel.dart
+++ giiku_camp_vol11_flutter_app/lib/zunda_room/zunda_room_viewmodel.dart
@@ -102,8 +102,7 @@ class ZundaRoomViewModel extends ChangeNotifier{
   final homeImages = HomeImages();
   static var home = HomeImages().home1[RoomDirection.left];
   late final ZundaMoveController controller;
-
-  static late Zundamon zundamon;
+  static Zundamon zundamon = Zundamon(Location(0,0), Image.asset("images/ZUNDA/zundamon1.png"), Axis.left, Status.stop);
 
   ZundaRoomViewModel(){
     Iterable<Widget> imageIte = ImageIte();
