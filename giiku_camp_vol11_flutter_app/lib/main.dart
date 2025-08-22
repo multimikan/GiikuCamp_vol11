109639f3276fb24119f71befca34f765ab7bf157
multimikan <koki20041008@gmail.com>
2025-08-22 10:36:05 +0900

    windowsizeをリサイズ

--- giiku_camp_vol11_flutter_app/lib/main.dart
+++ giiku_camp_vol11_flutter_app/lib/main.dart
@@ -12,6 +12,8 @@ class AppConfig {
   static late double windowHeight;
 }
 
+final RESIZE_WINDOW = 0.4;
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -44,8 +46,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
-    AppConfig.windowWidth = screenWidth;
-    AppConfig.windowHeight = screenHeight;
+    AppConfig.windowWidth = screenWidth*RESIZE_WINDOW;
+    AppConfig.windowHeight = screenHeight*RESIZE_WINDOW;
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
---
+++ giiku_camp_vol11_flutter_app/lib/zunda_room/image_helper.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:giiku_camp_vol11_flutter_app/main.dart';
+import 'package:giiku_camp_vol11_flutter_app/zunda_room/zunda_room_viewmodel.dart';
+
+class ImageHelper{
+  Image image;
+  int door_Y;
+  Map<String,int> floor_x; //[min:OO,maxOO]
+  Map<String,int> floor_y;
+  ImageHelper(this.image,this.door_Y,this.floor_x,this.floor_y);
+}
+
+class HomeImages{
+  final home1 = {
+    RoomDirection.left: ImageHelper(Image.asset("images/home1(L).png",fit: BoxFit.fill,),
+    (AppConfig.windowHeight*0.4).toInt(),
+  {"min":0,"max":(AppConfig.windowWidth*0.8).toInt()},
+  {"min":(AppConfig.windowHeight*0.4).toInt(),"max":AppConfig.windowHeight.toInt()}),
+
+    RoomDirection.right: ImageHelper(Image.asset("images/home1(R).png",fit: BoxFit.fill,),
+    (AppConfig.windowHeight*0.4).toInt(),
+  {"min":(AppConfig.windowWidth*0.2).toInt(),"max":(AppConfig.windowWidth*0.8).toInt()},
+  {"min":(AppConfig.windowHeight*0.4).toInt(),"max":AppConfig.windowHeight.toInt()}),
+
+  RoomDirection.center: ImageHelper(Image.asset("images/home1(C).png",fit: BoxFit.fill,),
+    (AppConfig.windowHeight*0.4).toInt(),
+  {"min":(AppConfig.windowWidth).toInt(),"max":(AppConfig.windowWidth*0.8).toInt()},
+  {"min":(AppConfig.windowHeight*0.4).toInt(),"max":AppConfig.windowHeight.toInt()}),
+  };
+
+  final home2 = {
+    RoomDirection.left: ImageHelper(Image.asset("images/home2(L).png",fit: BoxFit.fill,),
+    (AppConfig.windowHeight*0.4).toInt(),
+  {"min":0,"max":(AppConfig.windowWidth*0.8).toInt()},
+  {"min":(AppConfig.windowHeight*0.4).toInt(),"max":AppConfig.windowHeight.toInt()}),
+
+    RoomDirection.right: ImageHelper(Image.asset("images/home2(R).png",fit: BoxFit.fill,),
+    (AppConfig.windowHeight*0.4).toInt(),
+  {"min":(AppConfig.windowWidth*0.2).toInt(),"max":(AppConfig.windowWidth*0.8).toInt()},
+  {"min":(AppConfig.windowHeight*0.4).toInt(),"max":AppConfig.windowHeight.toInt()}),
+
+  RoomDirection.center: ImageHelper(Image.asset("images/home2(C).png",fit: BoxFit.fill,),
+    (AppConfig.windowHeight*0.4).toInt(),
+  {"min":(AppConfig.windowWidth).toInt(),"max":(AppConfig.windowWidth*0.8).toInt()},
+  {"min":(AppConfig.windowHeight*0.4).toInt(),"max":AppConfig.windowHeight.toInt()}),
+  };
+}
