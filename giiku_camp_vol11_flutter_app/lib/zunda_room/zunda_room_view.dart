8b156ddc225eac914e550a3a6e45d18d92797eef
multimikan <koki20041008@gmail.com>
2025-08-23 20:57:34 +0900

    Update zunda_room_view.dart

--- giiku_camp_vol11_flutter_app/lib/zunda_room/zunda_room_view.dart
+++ giiku_camp_vol11_flutter_app/lib/zunda_room/zunda_room_view.dart
@@ -176,15 +176,13 @@ class _ZundaRoomViewState extends State<ZundaRoomView> {
                 Column(
                   children: [
                 ZundamonWidget(),      // 位置基準はここ
-                Positioned(
-                  child: AnimatedSwitcher(
+                AnimatedSwitcher(
                     duration: Duration(milliseconds: 0),
                     child: ObjIcon(
                       key: ValueKey(ZundaRoomViewModel.zundamon.have), // 変更を判定するキー
                       obj: ZundaRoomViewModel.zundamon.have,
                     ),
                   ) ,
-                ),
                   ]
                 )
               ],
