6b5f839f75b882cad52031fbdfc51b54a1fda8e9
multimikan <koki20041008@gmail.com>
2025-08-24 06:55:20 +0900

    Update zunda_room_viewmodel.dart

--- giiku_camp_vol11_flutter_app/lib/zunda_room/zunda_room_viewmodel.dart
+++ giiku_camp_vol11_flutter_app/lib/zunda_room/zunda_room_viewmodel.dart
@@ -247,7 +247,7 @@ class ZundaMoveController extends ChangeNotifier{
   }
 
   Future<void> move() async{
-    if(!isMoveing) return;
+    if(isMoveing) return;
     if(jobList.isNotEmpty){
       print("move start");
       isMoveing = true;
